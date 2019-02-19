19c2ff6355b3f7e876e0eba29fb418c17ad9fec2
Joseph Lui <joseph.lui2000@gmail.com>
2019-02-18 22:55:37 -0500

    Updates

---
+++ precache-manifest.97ce1a784d6c70f53a4c255512070aa5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "29b60d3505d62f76aed2",
+    "url": "/clickygame3/static/css/main.babcc8bf.chunk.css"
+  },
+  {
+    "revision": "29b60d3505d62f76aed2",
+    "url": "/clickygame3/static/js/main.29b60d35.chunk.js"
+  },
+  {
+    "revision": "7406b077c004bd860e9c",
+    "url": "/clickygame3/static/js/runtime~main.7406b077.js"
+  },
+  {
+    "revision": "56d055b86f8a68f553b6",
+    "url": "/clickygame3/static/js/2.56d055b8.chunk.js"
+  },
+  {
+    "revision": "14fc8e9a92a0dbefdd49eb1c84d8caf8",
+    "url": "/clickygame3/static/media/ernie2.14fc8e9a.jpeg"
+  },
+  {
+    "revision": "9f91a6b0da6775fee7fc5e2d4658a363",
+    "url": "/clickygame3/static/media/oscar2.9f91a6b0.jpg"
+  },
+  {
+    "revision": "0d79d5d57e5d614bc96b17f9fa69fe93",
+    "url": "/clickygame3/static/media/team2.0d79d5d5.jpeg"
+  },
+  {
+    "revision": "8a9ba48590271be446976b705c215636",
+    "url": "/clickygame3/static/media/background.8a9ba485.jpg"
+  },
+  {
+    "revision": "35a7577a8df85b2ea330edbe72e4a7c4",
+    "url": "/clickygame3/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/clickygame3/precache-manifest.97ce1a784d6c70f53a4c255512070aa5.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/clickygame3/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
